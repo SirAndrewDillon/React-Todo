bbdcd1e5efb894d19a10437d64937c9acd1f4c4b
Andrew Dillon <majik895@gmail.com>
2019-05-29 15:17:35 -0400

    added todoform.js

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,18 +1,13 @@
 import React from 'react';
+
 const TodoForm = props => {
-  return (
-    <form>
-      <input
-        onChange={props.handleTodoChange}
-        type="text"
-        name="todo"
-        value={props.value}
-        placeholder="...todo"
-      />
-      <button onClick={props.handleAddTodo}>Add Todo</button>
-      <button onClick={props.handleClearTodos}>Clear Completed</button>
-    </form>
-  );
-};
+    return (
+        <div className='todo-form-container'>
+            <input value={props.userInput} onChange={props.handleSubmit} onKeyPress={props.enterPressed} type='text' placeholder='what more to do?' />
+            <button onClick={props.buttonPressed}>Add Task</button>
+            <button className='clear' onClick={props.clearItem}>Clear Completed</button>
+        </div>
+    );
+}
 
 export default TodoForm;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,21 +1,22 @@
+import React from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+import './Todo.css';
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
 
-import React from 'react';
-import Todo from './Todo';
-
 const TodoList = props => {
-  return (
-    <div>
-      {props.todos.map(todo => (
-        <Todo
-          handleToggleComplete={props.handleToggleComplete}
-          key={todo.id}
-          todo={todo}
-        />
-      ))}
-    </div>
-  );
+    return (
+        <div className='todo-container'>
+            <h1>Neverending List of Things</h1>
+            {props.todoList.map(todo => (
+                <div key={todo.id} className='todo' style={{backgroundColor: todo.backgroundColor}} onClick={() => props.strikeDone(todo.id)}>
+                    <Todo completed={todo.completed} task={todo.task} /> 
+                </div>
+            ))}
+            <TodoForm userInput={props.userInput} buttonPressed={props.buttonPressed} enterPressed={props.enterPressed} handleSubmit={props.handleSubmit} clearItem={props.clearItem}/>
+        </div>
+    );
 };
 
 export default TodoList;
